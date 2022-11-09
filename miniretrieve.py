e1b27e5c82bdd9f9e361e0c9b32b4e08f3929cd5
martinoswald <oswalma5@students.zhaw.ch>
2022-11-09 20:55:16 +0100

    indexes documents and queries

--- miniretrieve.py
+++ miniretrieve.py
@@ -3,7 +3,8 @@ from collections import defaultdict
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 from nltk import download
-from nltk.stem import snowball
+from nltk.stem import SnowballStemmer
+from nltk import FreqDist
 
 PATH_DOCS = 'documents'
 PATH_QRIS = 'queries'
@@ -11,28 +12,24 @@ STEMMING = True
 STOPWORDS = True
 
 ########################################################################################################################
-# Load NLTK packages
+# Initiate NLTK
 ########################################################################################################################
 
 download('punkt')
 download('stopwords')
+stopwords = set(stopwords.words('english'))
+stemmer = SnowballStemmer('english')
 
 ########################################################################################################################
 # Initiate data-structures
 ########################################################################################################################
 
 invindex = defaultdict(lambda: defaultdict(int))
-non_index = defaultdict(lambda: defaultdict(int))
+non_invindex = defaultdict(lambda: defaultdict(int))
 queries = defaultdict(lambda: defaultdict(int))
 
 ########################################################################################################################
-# Load Stopwords
-########################################################################################################################
-
-stopwords = set(stopwords.words('english'))
-
-########################################################################################################################
-# Read documents
+# Index documents
 ########################################################################################################################
 
 for doc in Path(PATH_DOCS).iterdir():
@@ -41,4 +38,29 @@ for doc in Path(PATH_DOCS).iterdir():
     tokens = word_tokenize(doc_text)
     # eliminate stopwords
     tokens = [word for word in tokens if word.casefold() not in stopwords] if STOPWORDS else tokens
-    print(tokens)
+    # stem words
+    tokens = [stemmer.stem(token) for token in tokens] if STEMMING else tokens
+    # count words
+    freq_dist = FreqDist(tokens)
+    # fill datastructures
+    for token, freq in freq_dist.items():
+        invindex[token][doc] += freq
+        non_invindex[doc][token] += freq
+
+########################################################################################################################
+# Index queries
+########################################################################################################################
+
+for query in Path(PATH_QRIS).iterdir():
+    query_text = query.read_text()
+    # tokenize text
+    tokens = word_tokenize(query_text)
+    # eliminate stopwords
+    tokens = [word for word in tokens if word.casefold() not in stopwords] if STOPWORDS else tokens
+    # stem words
+    tokens = [stemmer.stem(token) for token in tokens] if STEMMING else tokens
+    # count words
+    freq_dist = FreqDist(tokens)
+    # fill datastructure
+    for token, freq in freq_dist.items():
+        queries[query][token] += freq
